965763f3ba63dba656cfcea15b3621d3043e860b
William <williamxie00@gmail.com>
2024-02-22 19:17:22 -0700

    reorganize gripper file, add set_compliance

--- magpie/gripper.py
+++ magpie/gripper.py
@@ -47,12 +47,37 @@ class Gripper:
         self.servojoint = 84
         #plug in last reference frame to camera position for z axis
         self.Camera2Ref = 63
-        
-    #this is before you attach your motors to the gripper       
+
+    #this is before you attach your motors to the gripper
     def setup(self):
         self.Finger1.set_goal_position(0)
         self.Finger2.set_goal_position(1023)
-        
+
+    def open_gripper(self):
+        open1 = int((self.Motor1theta_min+4)*1023/300)
+        open2 = int((self.Motor2theta_max-4)*1023/300)
+        self.Finger1.set_goal_position(open1)
+        self.Finger2.set_goal_position(open2)
+
+    def close_gripper(self):
+        close1 = int((self.Motor1theta_max-4)*1023/300)
+        close2 = int((self.Motor2theta_min+4)*1023/300)
+        self.Finger1.set_goal_position(close1)
+        self.Finger2.set_goal_position(close2)
+
+    def theta_limit(self, delta_theta):
+        Motor1_theta = -delta_theta + self.Motor1theta_90
+        Motor2_theta = delta_theta + self.Motor2theta_90
+        if (Motor1_theta > self.Motor1theta_max or  Motor1_theta < self.Motor1theta_min):
+            return 1
+        if (Motor2_theta > self.Motor2theta_max or  Motor2_theta < self.Motor2theta_min):
+            return 1
+        return 0
+
+    def distance_from_ref(self, delta_theta):
+        delta_Z = self.Crank*math.cos(math.radians(delta_theta)) - 14 + 81.32 + self.Camera2Ref
+        return delta_Z
+
     def perform_action_on_fingers(self, action_func_name, arg, finger='both'):
         if finger == 'both':
             return [getattr(self.Finger1, action_func_name)(arg),
@@ -62,7 +87,7 @@ class Gripper:
         elif finger == 'right':
             return getattr(self.Finger2, action_func_name)(arg)
 
-    # general flow: desired mm distance 
+    # general flow: desired mm distance
     # --> call distance_to_theta(mm)
     # --> call position(theta) to move gripper
 
@@ -85,14 +110,14 @@ class Gripper:
         parallel_constant = (self.Finger1theta_90 if finger=='left' else self.Finger2theta_90)
         theta = (delta_theta * sign) + parallel_constant
         pos = int(theta * 1023 / 300)
-        return pos 
-    
+        return pos
+
     def position_to_theta(self, position, finger='left'):
         sign = (-1.0 if finger=='left' else 1.0)
         parallel_constant = (self.Finger1theta_90 if finger=='left' else self.Finger2theta_90)
         theta = (sign * (position * 300 / 1023.0)) - parallel_constant
         return theta
-    
+
     def set_distance(self, distance, finger='both'):
         if finger=='both':
             theta = self.distance_to_theta(distance)
@@ -120,25 +145,20 @@ class Gripper:
         self.apply_to_fingers('set_moving_speed', speedLimit, finger=finger)
         # self.Finger1.set_moving_speed(speedLimit)
         # self.Finger2.set_moving_speed(speedLimit)
-        
-    def theta_limit(self,delta_theta):
-        Motor1_theta = -delta_theta + self.Motor1theta_90
-        Motor2_theta = delta_theta + self.Motor2theta_90
-        if (Motor1_theta > self.Motor1theta_max or  Motor1_theta < self.Motor1theta_min):
-            return 1
-        if (Motor2_theta > self.Motor2theta_max or  Motor2_theta < self.Motor2theta_min):
-            return 1
-        return 0
-    
-    def distance_from_ref(self, delta_theta):
-        delta_Z = self.Crank*math.cos(math.radians(delta_theta)) - 14 + 81.32 + self.Camera2Ref
-        return delta_Z
+
+    def set_compliance(self, margin, flexibility, finger='both'):
+        self.apply_to_fingers('set_compliance_margin', margin, finger=finger)
+        self.apply_to_fingers('set_compliance_slope', flexibility, finger=finger)
+        # self.Finger1.set_compliance_margin(margin)
+        # self.Finger1.set_compliance_slope(flexibility)
+        # self.Finger2.set_compliance_margin(margin)
+        # self.Finger2.set_compliance_slope(flexibility)
 
     def get_position(self, finger='both'):
         self.apply_to_fingers('get_present_position', finger=finger)
         # self.Finger1.get_present_position()
         # self.Finger2.get_present_position()
-    
+
     def get_distance(self, finger='both'):
         # perform inverse calculations of theta_to_position, distance_to_theta
         f1_theta = self.position_to_theta(self.get_position(finger='left'))
@@ -150,17 +170,11 @@ class Gripper:
         else:
             return f1_dist if finger=='left' else f2_dist
 
-    def open_gripper(self):
-        open1 = int((self.Motor1theta_min+4)*1023/300)
-        open2 = int((self.Motor2theta_max-4)*1023/300)
-        self.Finger1.set_goal_position(open1)
-        self.Finger2.set_goal_position(open2)
-
     def get_temp(self, finger='both'):
         self.apply_to_fingers('get_temperature', finger=finger)
         # self.Finger1.get_temperature()
         # self.Finger2.get_temperature()
-        
+
     def get_load(self, finger='both'):
         self.apply_to_fingers('get_load', finger=finger)
         # if finger=='both':
@@ -170,15 +184,15 @@ class Gripper:
         #     return self.Finger1.get_load()
         # elif finger=='right':
         #     return self.Finger2.get_load()
-        
+
     # copilot generated, need to fix
     def close_until_load(self, stop_position, stop_torque, finger='both'):
         if finger=='both':
             self.Finger1.set_goal_position(stop_position)
             self.Finger2.set_goal_position(stop_position)
-    
-    # convert unitless load values to load at gripper contact point
-    def load_to_Nm(self, load):
+
+    # convert unitless load values to force normal load at gripper contact point
+    def load_to_N(self, load):
         '''
         @param load: unitless load value in bits, 0-2048. 0-1023 represents CW load, 1024-2047 represents CCW load
         '''
@@ -186,22 +200,22 @@ class Gripper:
         # see eqn on p17, figure 14 on p18 of: https://www.proquest.com/docview/2868478510?%20
         if load > 1023:
             load = load - 1023
-        Nm = -0.00001889 * load**2 + 0.038399 * load - 3.4073
+        N = -0.00001889 * load**2 + 0.038399 * load - 3.4073
         if load < 100:
             # made up polynomial to approximate the low load region
-            Nm = 0.0025 * load - 0.07 * load**2
-        return Nm
-    
-    def Nm_to_load(self, Nm):
+            N = 0.0025 * load - 0.07 * load**2
+        return N
+
+    def N_to_load(self, N):
         '''
-        @param Nm: load in Nm
+        @param N: force normal load at finger contact in N
         '''
         # derived by Stephen Otto empirically
         # see eqn on p17, figure 14 on p18 of: https://www.proquest.com/docview/2868478510?%20
-        load = 0.00001889 * Nm**2 - 0.038399 * Nm + 3.4073
+        load = 0.00001889 * N**2 - 0.038399 * N + 3.4073
         if load < 100:
             # made up polynomial to approximate the low load region
-            load = 0.0025 * Nm - 0.07 * Nm**2        
+            load = 0.0025 * N - 0.07 * N**2
         return load
 
     def disconnect(self):
@@ -211,7 +225,7 @@ if __name__ =="__main__":
     Controller = Gripper()
     Controller.openGripper()
     while True:
-        
+
         width_object = int(input("Input Width"))
         delta_theta = Controller.distance_to_theta(width_object)
         error = Controller.thetaLimit(delta_theta)
