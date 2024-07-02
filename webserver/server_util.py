1d627fbf76d4f545aadb5afcaf033ae789a0fc06
badinkajink <williamxie00@gmail.com>
2024-07-02 14:00:09 -0600

    add applied force to training data

--- webserver/server_util.py
+++ webserver/server_util.py
@@ -73,17 +73,19 @@ def log_to_df(path="robot_logs/df_combined.csv", timestamp=0):
     grasp_log = pd.DataFrame(gl.tolist()) # convert to pd df
     # keep only timestamp, aperture, gripper_vel, and contact_force cols, add actual_qd columns from actual_qd_0 to actual_qd_6 from the last row of move to grasp_log
 
-    grasp_log = grasp_log[['timestamp', 'aperture', 'gripper_vel', 'contact_force']]
+    grasp_log = grasp_log[['timestamp', 'aperture', 'gripper_vel', 'contact_force', 'applied_force']]
     grasp_log[['actual_qd_0', 'actual_qd_1', 'actual_qd_2', 'actual_qd_3', 'actual_qd_4', 'actual_qd_5']] = move.iloc[-1][['actual_qd_0', 'actual_qd_1', 'actual_qd_2', 'actual_qd_3', 'actual_qd_4', 'actual_qd_5']]
-    grasp_log = grasp_log[['timestamp', 'actual_qd_0', 'actual_qd_1', 'actual_qd_2', 'actual_qd_3', 'actual_qd_4', 'actual_qd_5', 'aperture', 'gripper_vel', 'contact_force']]
+    grasp_log = grasp_log[['timestamp', 'actual_qd_0', 'actual_qd_1', 'actual_qd_2', 'actual_qd_3', 'actual_qd_4', 'actual_qd_5', 'aperture', 'gripper_vel', 'contact_force', 'applied_force']]
     
     # add columns to move and home dfs, zeroed out
     move['aperture'] = 0.0
     move['gripper_vel'] = 0.0
     move['contact_force'] = 0.0
+    move['applied_force'] = 0.0
     home['aperture'] = 0.0
     home['gripper_vel'] = 0.0
     home['contact_force'] = 0.0
+    home['applied_force'] = 0.0
 
 
     # get initial values, iteratively subtract 0.1s from ti and assign to to move timestamps, from last to first
@@ -99,14 +101,16 @@ def log_to_df(path="robot_logs/df_combined.csv", timestamp=0):
     # get last timestamp tf, iteratively add 0.1s to tf and assign to to home timestamps, from first to last
     tf = gl[-1]['timestamp']
     af = gl[-1]['aperture']
-    cf = gl[-1]['contact_force']
+    cff = gl[-1]['contact_force']
+    aff = gl[-1]['applied_force']
 
     for i in np.arange(len(home)):
         # iterate forwards through home timestamps
         tf += 0.1
         home.loc[i, 'timestamp'] = tf
         home.loc[i, 'aperture'] = af
-        home.loc[i, 'contact_force'] = cf
+        home.loc[i, 'contact_force'] = cff
+        home.loc[i, 'applied_force'] = aff
 
     # combine dataframes vertically
     df = pd.concat([move, grasp_log, home], axis=0)
@@ -134,6 +138,7 @@ def df_to_rlds(df, img, path="robot_logs/episode.tfds"):
                 'aperture': step['aperture'],
                 'gripper_vel': step['gripper_vel'],
                 'contact_force': step['contact_force'],
+                'applied_force': step['applied_force'],
                 'image': np.array(img)
             },
             'action': 1,  # Placeholder, assuming no action data is available
@@ -166,6 +171,7 @@ def df_to_rlds(df, img, path="robot_logs/episode.tfds"):
             'aperture': tf.float32,
             'gripper_vel': tf.float32,
             'contact_force': tf.float32,
+            'applied_force': tf.float32,
             'image': tf.uint8
         },
         tf.float32,  # action
@@ -180,6 +186,7 @@ def df_to_rlds(df, img, path="robot_logs/episode.tfds"):
             'aperture': tf.TensorShape([]),
             'gripper_vel': tf.TensorShape([]),
             'contact_force': tf.TensorShape([]),
+            'applied_force': tf.TensorShape([]),
             'image': tf.TensorShape([480, 640, 3])
         },
         tf.TensorShape([]),  # action
